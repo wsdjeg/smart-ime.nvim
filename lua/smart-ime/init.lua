911e914b4169b2417c775bcbeac5be809386338c
Eric Wong <wsdjeg@outlook.com>
2025-12-17 17:38:12 +0800

    feat: init version

---
+++ lua/smart-ime/init.lua
@@ -0,0 +1,70 @@
+local M = {}
+
+function M.setup(opt)
+	local augroup = vim.api.nvim_create_augroup("smart-ime.nvim", { clear = true })
+
+	local log = require("logger").derive("eric")
+
+	local create_autocmd = vim.api.nvim_create_autocmd
+
+    local imselect = opt.imselect
+
+	local function imselect_cn()
+		vim.system({ imselect, "-k=ctrl+space", "中文模式" })
+	end
+
+	local function imselect_en()
+		vim.system({ imselect, "-k=ctrl+space", "英语模式" })
+	end
+
+	local buffer_im = {}
+
+	create_autocmd({ "InsertLeave" }, {
+		pattern = { "*" },
+		group = augroup,
+		callback = function(ev)
+			vim.system({ imselect }, { text = true }, function(o)
+				-- 这里说明下，再 Windows Terminal 内执行该命令输出的内容默认编码是 `cp936`,
+				-- 需要转码成 utf-8，同时，输出内容尾部有换行符，使用 trim 函数去除。
+				local m = vim.trim(vim.iconv(o.stdout, "cp936", "utf-8"))
+				log.info("save buffer insert mode: " .. m)
+				buffer_im[ev.buf] = m
+			end)
+			imselect_en()
+		end,
+	})
+	create_autocmd({ "InsertEnter" }, {
+		pattern = { "*" },
+		group = augroup,
+		callback = function(ev)
+			if buffer_im[ev.buf] and buffer_im[ev.buf] ~= "英语模式" then
+				-- 此处设置快捷键，可以在输入法按键设置里面查看，我选择的是使用 ctrl-space 切换中英文
+				-- 默认我记得是 shift，同时这个命令默认也是 `-k=shift`
+				log.info("change to " .. buffer_im[ev.buf])
+				vim.system({ imselect, "-k=ctrl+space", buffer_im[ev.buf] })
+			end
+		end,
+	})
+
+	create_autocmd({ "FocusGained" }, {
+		pattern = { "*" },
+		group = augroup,
+		callback = function(ev)
+			log.info(string.format("%s event is triggered", ev.event))
+			if vim.fn.mode() == "n" then
+				log.info("switch to english in normal mode")
+				imselect_en()
+			elseif vim.fn.mode() == "i" then
+				if buffer_im[ev.buf] == "英语模式" then
+					log.info("switch to english in insert mode")
+					imselect_en()
+				elseif buffer_im[ev.buf] == "中文模式" then
+					log.info("switch to chinese in insert mode")
+					imselect_cn()
+				end
+			end
+		end,
+	})
+end
+
+return M
